acaf53dfe1ac3e5f2125343835ec6365f9cd4e9f
alvin joseph valdez <alvinjosephvaldez@Macintoshs-MacBook-Pro-3.local>
2018-10-30 00:22:35 +0800

    Fresh Meat

---
+++ neermo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  neermo
+//
+//  Created by alvin joseph valdez on 30/10/2018.
+//  Copyright © 2018 alvin joseph valdez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
